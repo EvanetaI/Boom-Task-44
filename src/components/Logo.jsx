c785606763e4a10d61af229363748fed3f3828c9
Evaneta <eftimovaevaneta@gmail.com>
2023-02-22 22:28:17 +0200

    Delete Logo.jsx

--- src/components/Logo.jsx
+++
@@ -1,9 +0,0 @@
-export default function Logo(props) {
-    if(!props.type) props.type = "default"
-
-    return (
-      <div>
-        {props.type === "default" ? <img src="/images/logo-muted.svg"></img> : <img src="/images/logo.svg"></img>}
-      </div>
-    );
-  }
